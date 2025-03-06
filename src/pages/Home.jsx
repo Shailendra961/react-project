d107d2e14e00643891003779629aec73b62e81df
Shailendra <Shailendra_Rathore>
2025-03-06 13:55:12 +0530

    change heading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home =()=>
         return(
             <>
             
-                <h1 className="text-3xl font-bold underline text-center my-6">Welcome to Home Page</h1>
+                {/* <h1 className="text-3xl font-bold underline text-center my-6">Welcome to Home Page</h1> */}
                 <CountryFlags/>
             </>
         )
